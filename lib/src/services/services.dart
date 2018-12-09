f41479f0c5c31701536c3cd02e87f8371b35a83d
Flavio Farias <flavio@engapp.com.br>
2018-12-09 04:32:52 -0300

    create services archive

---
+++ lib/src/services/services.dart
@@ -0,0 +1,5 @@
+class Services {
+  static String basic = 'https://jsonplaceholder.typicode.com/albums';
+  static String advanced = 'https://jsonplaceholder.typicode.com/users';
+  static String complex = 'https://reqres.in/api/users';
+}
