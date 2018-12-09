f8254faf3d64bbd7a9c975513c6fdf9c8e0d9dfa
Flavio Farias <flavio@engapp.com.br>
2018-12-09 17:38:14 -0300

    update services archive

--- lib/src/services/services.dart
+++ lib/src/services/services.dart
@@ -1,5 +1,5 @@
 class Services {
-  static String basic = 'https://jsonplaceholder.typicode.com/albums';
+  static String basic = 'https://jsonplaceholder.typicode.com/photos';
   static String advanced = 'https://jsonplaceholder.typicode.com/users';
   static String complex = 'https://reqres.in/api/users';
 }
